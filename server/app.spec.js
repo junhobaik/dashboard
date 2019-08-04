5ddaa41a9d59c5a0bf00df5bf1feff6428252103
Junho Baik <junhobaik@gmail.com>
2019-08-04 21:36:42 +0900

    server, 테스트 코드 골격 추가

---
+++ server/app.spec.js
@@ -0,0 +1,10 @@
+const app = require("./app.js");
+const request = require("supertest");
+
+describe("", () => {
+  it("", done => {
+    request(app).end((err, res) => {
+      done();
+    });
+  });
+});
