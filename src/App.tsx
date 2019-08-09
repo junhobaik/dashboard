8fae6aafe2bd894646207cc95aba593594fcdbfe
Junho Baik <junhobaik@gmail.com>
2019-08-10 00:02:30 +0900

    App 구조 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+export interface AppProps {}
+export interface AppStates {
+  foo: boolean;
+}
+
+class App extends React.Component<AppProps, AppStates> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      foo: true
+    };
+  }
+
+  render() {
+    return <div id="App"/>;
+  }
 }
 
 export default App;
