0013dd0c989f1b718ffa03dce40d86a368f3e7bd
Junho Baik <junhobaik@gmail.com>
2019-08-12 21:42:38 +0900

    CORS 이슈 해결을 위한 cors 패키지 적용

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 require('dotenv').config();
 
@@ -8,6 +9,8 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema');
 
 const app = express();
+
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
