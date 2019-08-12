36cc129b305092b5a3a77f37a9a1ddeece802ec5
Junho Baik <junhobaik@gmail.com>
2019-08-12 22:07:38 +0900

    User, 유저 페이지 생성 및 구현

---
+++ src/components/User/index.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+
+import { USER_DATA } from '../../queries';
+
+interface iProps {
+  match: any;
+}
+interface iStates {}
+
+export default class User extends Component<iProps, iStates> {
+  render() {
+    const { match } = this.props;
+    const { userId } = match.params;
+
+    return (
+      <Query query={USER_DATA} variables={{ userId: parseInt(userId, 10) }}>
+        {({ loading, data, error }: any) => {
+          if (loading) return <span>loading</span>;
+          if (error) return <span>error</span>;
+
+          const { name } = data.user;
+          return (
+            <div id="User">
+              <div className="home-inner">{`${name}님 환영합니다.`}</div>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
