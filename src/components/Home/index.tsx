0679912c9614d1da4d8f20cb83d79ea712cfbb73
Junho Baik <junhobaik@gmail.com>
2019-08-12 22:07:17 +0900

    Home, 유저 정보 페이지를 만듬에 따른 초기화

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,24 +1,7 @@
 import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-
-import { HOME_PAGE } from '../../queries';
 
 export default class Home extends Component {
   render() {
-    return (
-      <Query query={HOME_PAGE}>
-        {({ loading, data, error }: any) => {
-          if (loading) return <span>loading</span>;
-          if (error) return <span>error</span>;
-
-          const { name } = data.user;
-          return (
-            <div id="Home">
-              <div className="home-inner">{`${name}님 환영합니다.`}</div>
-            </div>
-          );
-        }}
-      </Query>
-    );
+    return <div id="Home">Home</div>;
   }
 }
