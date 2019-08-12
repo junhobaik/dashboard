2647e570895fbe38cc7678f068410f6ca5a0dade
Junho Baik <junhobaik@gmail.com>
2019-08-12 22:08:25 +0900

    App, router 관련 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 
 import client from './apolloClient';
 import './App.scss';
 
 import Home from './components/Home';
+import User from './components/User';
+import Admin from './components/Admin';
 
 export interface AppProps {}
 export interface AppStates {}
@@ -15,7 +17,9 @@ class App extends React.Component<AppProps, AppStates> {
     return (
       <ApolloProvider client={client}>
         <Router>
-          <Route exact={true} path={`/`} component={Home} />
+            <Route exact={true} path={`/`} component={Home} />
+            <Route path={`/user/:userId`} component={User} />
+            <Route path={`/admin`} component={Admin} />
         </Router>
       </ApolloProvider>
     );
