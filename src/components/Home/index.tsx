62136fe3ed4b8a460865e636101cc686fb4fb515
Junho Baik <junhobaik@gmail.com>
2019-08-16 01:58:24 +0900

    Home, passport google 로그인과 연동 테스트를 위한 수정

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
+  componentDidMount() {
+    const accountBtn: any = document.querySelector('#check-account');
+    accountBtn.addEventListener('click', () => {
+      fetch('/api/account')
+        .then(res => res.json())
+        .then(json => {
+          console.log(json);
+        });
+    });
+  }
   render() {
-    return <div id="Home">Home</div>;
+    return (
+      <div id="Home">
+        <div className="login">
+          <a href="/auth/google">Google Login</a>
+        </div>
+        <button id="check-account">Account 정보 확인 (console)</button>
+      </div>
+    );
   }
 }
