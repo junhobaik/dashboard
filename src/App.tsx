7e724d2bbad3b545ad6cdd9e41b4bdfb7eeda800
Junho Baik <junhobaik@gmail.com>
2019-08-21 11:35:22 +0900

    App, Header 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.scss';
 import Home from './components/Home';
 import User from './components/User';
 import Admin from './components/Admin';
+import Header from './components/Header';
 
 export interface AppProps {}
 export interface AppStates {}
@@ -16,10 +17,11 @@ class App extends React.Component<AppProps, AppStates> {
   render() {
     return (
       <ApolloProvider client={client}>
+        <Header />
         <Router>
-            <Route exact={true} path={`/`} component={Home} />
-            <Route path={`/user`} component={User} />
-            <Route path={`/admin`} component={Admin} />
+          <Route exact={true} path={`/`} component={Home} />
+          <Route path={`/user`} component={User} />
+          <Route path={`/admin`} component={Admin} />
         </Router>
       </ApolloProvider>
     );
