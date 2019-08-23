b46cbc8ac34991f371b3e82dc6219fb525a2a158
Junho Baik <junhobaik@gmail.com>
2019-08-24 00:02:52 +0900

    이전 ts 파일 삭제

--- src_old/setupProxy.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable func-names */
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy('/auth/*', { target: 'http://localhost:4000', logLevel: "debug" }));
-  app.use(proxy('/api/*', { target: 'http://localhost:4000', logLevel: "debug" }));
-};
