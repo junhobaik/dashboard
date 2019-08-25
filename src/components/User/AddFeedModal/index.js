a4a95722f530aaf9bc88ed7f702b8526d5f85b49
Junho Baik <junhobaik@gmail.com>
2019-08-26 00:48:53 +0900

    AddFeedModal 프론트 마크업 구현
    - Feed URL 입력 input,
    - Feed Category 선택 또는 새 카테고리

--- src/components/User/AddFeedModal/index.js
+++ src/components/User/AddFeedModal/index.js
@@ -1,21 +1,90 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon as Fa } from '@fortawesome/react-fontawesome';
+import { faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import './index.scss';
 
-const AddFeedModal = ({ isOpen, close }) => {
-  if (isOpen) {
-    return (
-      <React.Fragment>
-        <div className="modal-layout" onClick={close} role="button" tabIndex="0" />
-        <div id="AddFeedModal">
-          <h1>Add Feed</h1>
-        </div>
-      </React.Fragment>
-    );
+class AddFeedModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAddCategory: false
+    };
   }
-  return null;
-};
+
+  toggleCategoryInput = () => {
+    const { isAddCategory } = this.state;
+    this.setState({
+      isAddCategory: !isAddCategory
+    });
+  };
+
+  render() {
+    console.log('render');
+    const { isOpen, close } = this.props;
+    const { isAddCategory } = this.state;
+
+    if (isOpen) {
+      return (
+        <React.Fragment>
+          <div className="modal-layout" onClick={close} role="button" tabIndex="0" />
+          <div id="AddFeedModal">
+            <div className="header">
+              <h1 className="title">Add Feed</h1>
+              <button type="button" className="close" onClick={close}>
+                <Fa icon={faTimes} />
+              </button>
+            </div>
+            <div className="content">
+              <div className="feed-link">
+                <div className="link-input">
+                  <h2>Feed URL</h2>
+                  <input type="text" id="feedLink" placeholder="ex. http://site.com/rss" />
+                </div>
+
+                <div className="link-msg">
+                  <div className="alert alert-info" role="alert">
+                    Feed 주소 또는 해당 사이트 주소를 입력해주세요.
+                  </div>
+                </div>
+              </div>
+              <div className="feed-category">
+                <h2>Category</h2>
+                <div className="category-select">
+                  {!isAddCategory ? (
+                    <select name="category">
+                      <option value="">category</option>
+                      <option value="foo1">foo1</option>
+                      <option value="foo2">foo2</option>
+                      <option value="foo3">foo3</option>
+                    </select>
+                  ) : (
+                    <input type="text" id="newCategory" placeholder="category name" />
+                  )}
+
+                  <div className="add-category-icon">
+                    <Fa
+                      className="add-category-btn"
+                      icon={!isAddCategory ? faPlus : faTimes}
+                      onClick={this.toggleCategoryInput}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="submit">
+              <button type="button" className="btn btn-primary">
+                Add Feed
+              </button>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    }
+    return null;
+  }
+}
 
 AddFeedModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
