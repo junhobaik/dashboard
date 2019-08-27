392d475014cd01d7d09e144aa44afc51f7df42c8
Junho Baik <junhobaik@gmail.com>
2019-08-28 00:22:58 +0900

    userModel, mongoose 스키마 수정

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
-  googleId: { type: String }
+  googleId: { type: String, required: true },
+  feedList: [{ feedId: String, title: String, category: String, readedItem: [String] }]
 });
 
 export default mongoose.model('user', UserSchema);
