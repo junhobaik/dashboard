077dc622f72b7f4b9b4fc00da9635268a6c583e0
Junho Baik <junhobaik@gmail.com>
2019-08-31 00:43:00 +0900

    ApolloServer와 ApolloClient간 Context 연결 안되는 문제 해결

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,24 @@
 import ApolloClient from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers
+    }
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  credentials: 'include'
 });
 
 export default client;
