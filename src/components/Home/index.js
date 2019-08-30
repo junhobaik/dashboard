0fc98595b170c0b4bfb69dec7f82675eb93853f0
Junho Baik <junhobaik@gmail.com>
2019-08-31 01:28:15 +0900

    login 상태가 아니라면 intro로 redirect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+
 import './index.scss';
+import { USER_DATA } from '../../queries';
 
 const Home = () => {
-  return <div id="Home">Home</div>;
+  return (
+    <div id="Home">
+      <Query query={USER_DATA}>
+        {({ loading, data, error }) => {
+          console.log('Home <Qeury />', loading, data, error);
+
+          if (loading) return <span>Loding...</span>;
+          if (error) return <span>Error..!</span>;
+
+          if (data && data.user) {
+            return <span>Home</span>;
+          }
+          
+          return <Redirect to="/intro" />;
+        }}
+      </Query>
+    </div>
+  );
 };
 
 export default Home;
