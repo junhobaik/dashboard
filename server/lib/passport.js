61cdb3275b695e0f0ad863903a55a47473841c8a
Junho Baik <junhobaik@gmail.com>
2019-09-01 22:07:27 +0900

    불필요한 코드 제거

--- server/lib/passport.js
+++ server/lib/passport.js
@@ -1,28 +1,12 @@
-/* eslint-disable no-unused-vars */
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { userModel } from '../models';
 
 module.exports = passport => {
-  // passport
   passport.serializeUser((user, done) => {
-    // console.log('> serializeUser', user.displayName);
     done(null, user);
   });
 
   passport.deserializeUser((user, done) => {
-    // console.log('> deserializeUser:', user.displayName);
     done(null, user);
-    // userModel.findOne({ googleId: user.id }, (err, userData) => {
-    //   if (userData) {
-    //     done(null, userData);
-    //   } else {
-    //     userModel
-    //       .create({ googleId: user.id, name: user.displayName })
-    //       .then((createdUser, error) => {
-    //         done(null, createdUser);
-    //       });
-    //   }
-    // });
   });
 
   passport.use(
