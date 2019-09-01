d1d43c9b13d587fc8e06ce532304ce7a7b7f0d8a
Junho Baik <junhobaik@gmail.com>
2019-09-02 07:08:34 +0900

    mongoose user schema, picture 추가

--- server/models/user.js
+++ server/models/user.js
@@ -5,18 +5,18 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   googleId: { type: String, required: true },
+  picture: { type: String },
   feedList: [{ feedId: String, title: String, category: String, readedItem: [String] }]
 });
 
-UserSchema.pre('save', async next => {
-  try {
-    const user = this;
-    console.log('userSchema pre:', user);
-
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-});
-
 export default mongoose.model('user', UserSchema);
+
+// UserSchema.pre('save', async next => {
+//   try {
+//     const user = this;
+//     console.log('userSchema pre:', user);
+//     return next();
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
