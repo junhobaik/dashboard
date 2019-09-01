cc41c4989db31712c3cfc2a81cf4c80b6e1d7859
Junho Baik <junhobaik@gmail.com>
2019-09-02 07:07:56 +0900

    구글 로그인 시 데이터 create(가입)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,17 +8,31 @@ const router = express.Router();
 
 const clientPath = 'http://localhost:3000';
 
-router.get(
-  '/google',
-  passport.authenticate('google', { scope: ['profile'] }),
-  (req, res) => {}
-);
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }), (req, res) => {});
 
 router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: clientPath }),
   (req, res) => {
-    res.redirect(`${clientPath}/board`);
+    const { id, displayName } = req.user;
+    const { picture } = req.user._json;
+
+    userModel.findOne({ googleId: id }, (err, userData) => {
+      if (err) return new Error(err);
+      if (!userData) {
+        userModel
+          .create({ googleId: id, name: displayName, picture })
+          .then((created, error) => {
+            return created;
+          })
+          .finally(() => {
+            res.redirect(`${clientPath}/board`);
+          });
+      } else {
+        res.redirect(`${clientPath}`);
+      }
+      return null;
+    });
   }
 );
 
