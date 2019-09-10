261da450fbddc53b7d3ed55b7816dabb0bb83dc8
Junho Baik <junhobaik@gmail.com>
2019-09-10 22:33:47 +0900

    feed items를 특정 시간마다 업데이트하는 기능 구현
    - 현재 10분 interval 설정

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-unused-vars */
-/* eslint-disable consistent-return */
+
 import express from 'express';
 import Parser from 'rss-parser';
+import moment from 'moment';
 
-import { feedModel, userModel } from '../models';
+import { feedModel } from '../models';
 import isAuthenticated from './util';
 
 const router = express.Router();
@@ -25,14 +27,73 @@ router.get('/getfeed', (req, res, next) => {
   const { url } = req.query;
 
   getFeed(url)
-    .then(() => {
-      // console.log('Success');
+    .then(feed => {
       res.sendStatus(200);
     })
     .catch(err => {
-      // console.log('Error');
       res.sendStatus(204);
     });
 });
+
+const updateFeed = () => {
+  console.log('interval');
+  feedModel
+    .find({}, { feedUrl: true, pubDate: true, syncDate: true })
+    .then(pubDates => {
+      const feedList = [];
+      for (const { pubDate, feedUrl, _id } of pubDates) {
+        feedList.push({ feedUrl, pubDate });
+      }
+      return feedList;
+    })
+    .then(feedList => {
+      const pipe = (...funcs) => argument => funcs.reduce((acc, func) => func(acc), argument);
+
+      const updateList = async ({ feedUrl, pubDate }) => {
+        const result = await parser.parseURL(feedUrl).then(f => {
+          const rPubDate = f.pubDate ? new Date(f.pubDate) : null || f.lastBuildDate;
+
+          if (rPubDate > pubDate) {
+            console.log('');
+            console.log(`- ${f.title}: 업데이트 필요`);
+            console.log(`  - SITE: ${moment(rPubDate).fromNow()}`);
+            console.log(`  - DATA: ${moment(pubDate).fromNow()}`);
+
+            feedModel
+              .updateOne(
+                { feedUrl },
+                {
+                  $set: {
+                    pubDate: rPubDate,
+                    items: f.items
+                  }
+                }
+              )
+              .then(res => {
+                console.log(`${res.nModified}개 업데이트`);
+              });
+
+            return feedUrl;
+          }
+          console.log('');
+          console.log(`- ${f.title}: 업데이트 불필요`);
+          return feedUrl;
+        });
+        return result;
+      };
+
+      const fns = [];
+      for (const { feedUrl, pubDate } of feedList) {
+        fns.push(async () => await updateList({ feedUrl, pubDate }));
+      }
+
+      const checkAndUpdate = pipe(...fns);
+      checkAndUpdate();
+    });
+};
+// updateFeed();
+const interval = setInterval(() => {
+  updateFeed();
+}, 10 * 60 * 1000);
 
 export default router;
